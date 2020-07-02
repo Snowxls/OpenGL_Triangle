1c1b9378723a3404f6b38aba663ff0d6ba56432b
Warlock.Snow <snow@SnowdeMacBook-Pro.local>
2020-07-02 20:03:36 +0800

    第一个三角形

---
+++ OpenGL_DemoTests/OpenGL_DemoTests.m
@@ -0,0 +1,37 @@
+//
+//  OpenGL_DemoTests.m
+//  OpenGL_DemoTests
+//
+//  Created by Snow WarLock on 2020/6/29.
+//  Copyright © 2020 Snowxls. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OpenGL_DemoTests : XCTestCase
+
+@end
+
+@implementation OpenGL_DemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
